fc3133ed13ccd7645c54b04057a8d3820a7de747
oriel <oriel.absin@gmail.com>
2017-02-01 11:11:54 +0800

    add tooltip

--- public/comp/m/MyCustomChart.js
+++ public/comp/m/MyCustomChart.js
@@ -13,6 +13,7 @@ export default class MyCustomChart {
         }
 
         create(data) {
+
         var svg = d3.select(this.el).append('svg')
             .attr("width", 600)
             .attr("height", 200),
@@ -156,6 +157,130 @@ export default class MyCustomChart {
                 
                 return lr;
         }
+
+/////////////TOOLTIP////////////////
+
+        var focus = svg.append("g") 
+            .style("display", "none");
+        
+        var bisectDate = d3.bisector(function(d) { return d.date; }).left;
+
+        // append the x line
+        focus.append("line")
+            .attr("class", "x")
+            .style("stroke", "red")
+            .style("stroke-dasharray", "3,3")
+            .style("opacity", 0.5)
+            .attr("y1", 0)
+            .attr("y2", height);
+
+        // append the y line
+        focus.append("line")
+            .attr("class", "y")
+            .style("stroke", "red")
+            .style("stroke-dasharray", "3,3")
+            .style("opacity", 0.5)
+            .attr("x1", width)
+            .attr("x2", width);
+
+        // append the circle at the intersection
+        focus.append("circle")
+            .attr("class", "y")
+            .style("fill", "none")
+            .style("stroke", "red")
+            .attr("r", 8);
+
+        // place the value at the intersection
+        focus.append("text")
+            .attr("class", "y1")
+            .style("stroke", "white")
+            .style("stroke-width", "3.5px")
+            .style("opacity", 0.8)
+            .attr("dx", 8)
+            .attr("dy", "-.3em");
+        focus.append("text")
+            .attr("class", "y2")
+            .attr("dx", 8)
+            .attr("dy", "-.3em");
+
+        // place the date at the intersection
+        focus.append("text")
+            .attr("class", "y3")
+            .style("stroke", "white")
+            .style("stroke-width", "3.5px")
+            .style("opacity", 0.8)
+            .attr("dx", 8)
+            .attr("dy", "1em");
+        focus.append("text")
+            .attr("class", "y4")
+            .attr("dx", 8)
+            .attr("dy", "1em");
+        
+        // append the rectangle to capture mouse
+        svg.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .on("mouseover", function() { focus.style("display", null); })
+            .on("mouseout", function() { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
+
+        function mousemove() {
+            
+            var x0 = x.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+
+                console.log(data)
+
+            var eastValue = d.east - mean
+
+            focus.select("circle.y")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                                    y(eastValue) + ")");
+
+            focus.select("text.y1")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                                    y(eastValue) + ")")
+                .text(d.east);
+
+            focus.select("text.y2")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                                    y(eastValue) + ")")
+                .text(d.east);
+
+            focus.select("text.y3")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                                    y(eastValue) + ")")
+                .text(d.date);
+
+            focus.select("text.y4")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                                    y(eastValue) + ")")
+                .text(d.date);
+
+            focus.select(".x")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                                    y(eastValue) + ")")
+                        .attr("y2", height - y(eastValue));
+
+            focus.select(".y")
+                .attr("transform",
+                    "translate(" + width * -1 + "," +
+                                    y(eastValue) + ")")
+                        .attr("x2", width + width);
+        }
+
+
     }
 
     update() {

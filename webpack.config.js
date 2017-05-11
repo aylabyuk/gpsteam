22594e84ccf552655a0c2cd8dcce6807d250188c
oriel <oriel.absin@gmail.com>
2017-05-11 09:42:05 +0800

    remove dev config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
+var CompressionPlugin = require("compression-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
-    devtool: 'inline-source-map',
     entry: [
         'webpack-hot-middleware/client',
         './public/_primary.js',
@@ -13,28 +14,62 @@ module.exports = {
         publicPath: '/'
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                // This has effect on the react lib size
+                'NODE_ENV': JSON.stringify('production'),
+            }
+        }),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                warnings: false, // Suppress uglification warnings
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                screw_ie8: true
+            },
+            output: {
+                comments: false,
+            },
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        }),
+        new CleanWebpackPlugin(['dist/*.*'], { verbose: true, dry: false }),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin(),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0
+        })
     ],
     module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets: ['es2015', 'react', 'react-hmre', 'stage-1']
-                }
-            },
+        rules: [
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                use: [
+                   'style-loader',
+                   'css-loader'
+                ]
             },
-            { 
+            {
                 test: [/\.png$/, /\.eot$/, /\.woff2$/, /\.woff$/, /\.ttf$/, /\.svg$/],
-                loader: 'url-loader' 
+                loader: 'url-loader'
+            },
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015', 'react', 'react-hmre', 'stage-1']
+                }
+                
             }
         ]
     },
-    watch: true
 }
+
+// process.traceDeprecation = true

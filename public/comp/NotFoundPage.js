4129aab06dff43ba9f07d93bbedaddaa34dd06cf
oriel <oriel.absin@gmail.com>
2017-05-16 10:15:08 +0800

    add page not found

--- public/comp/NotFoundPage.js
+++ public/comp/NotFoundPage.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react'
 
+const style = {
+    position: 'absolute',
+    top: '20%',
+    left: '50%',
+    width: '30em',
+    height: '18em',
+    marginTtop: '-9em',
+    marginLeft: '-15em',
+    border: '20px solid #ccc',
+    backgroundColor: '#f3f3f3',
+}
+
 class NotFoundPage extends Component {
 	render() {
 		return (
-            <div>
+            <div style={style}>
+                <code>{this.props.location.pathname}</code>
                 <h1>404 - Page Not Found</h1>
-                <p>I'm sorry, the page you were looking for cannot be found!</p>
+                <h3>I'm sorry, the page you were looking for cannot be found!</h3>
             </div>
 		)
 	}
--- public/routes.js
+++ public/routes.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
 // routing
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 // components
 import _Logsheet from './comp/logsheet/_Logsheet'
 import _Staff from './comp/staff/_Staff'
+import NotFoundPage from './comp/NotFoundPage'
 
 const routes = () => {
     return(
         <div>
+            <Switch>
             <Route exact path='/' component={_Logsheet} />
             <Route path='/staff' component={_Staff} />
+
+            <Route component={NotFoundPage} />
+            </Switch>
         </div>
     )
 }

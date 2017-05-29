abd3b9ee26912891aa4a3a0a17ba2c1bf9388e8f
oriel <oriel.absin@gmail.com>
2017-05-29 14:44:12 +0800

    add tootip each hover

--- public/comp/map/Marker.js
+++ public/comp/map/Marker.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+const hoverStyle = 'circleBase hovered'
+const defaultStyle = 'circleBase normal'
+
+
 class Marker extends Component {
     render() {
+        const style = this.props.$hover ? defaultStyle : defaultStyle;
+        // console.log(this.props)
         return (
-            <div className='circleBase type1'>
-                
+            <div>
+                <div className='hoveredtext' style={{visibility: this.props.$hover ? 'visible' : 'hidden'}}>
+                    {this.props.sitename}
+                </div>
+                <div className={style} />
             </div>
         );
     }

cbcac37bf59a425d27c0cc5f61f20d5072fca1ec
oriel <oriel.absin@gmail.com>
2017-07-28 11:04:49 +0800

    organize logsheet by date on details

--- public/comp/dashboard/SiteDetailsDrawer.js
+++ public/comp/dashboard/SiteDetailsDrawer.js
@@ -26,7 +26,31 @@ class SiteDetailsDrawer extends Component {
         if(data.loading || site === "") {
             return <p>loading...</p>
         } else {
+
+            let years = []
+            let sortedLogsheets = []
+
+            data.sitesWithLogsheet[0].logsheets.map((l) => {
+                return sortedLogsheets.push(l)
+            })
+
+            sortedLogsheets.sort((a,b) => {
+                a = new Date(a.logsheet_date);
+                b = new Date(b.logsheet_date);
+                return a>b ? -1 : a<b ? 1 : 0;
+            })
+
+            sortedLogsheets.map((l)=> {
+                let ldate = new Date(l.logsheet_date)
+                if(!years.includes(ldate.getFullYear())) {
+                    years.push(ldate.getFullYear())
+                }
+            })
+
             return (
+
+                
+
                 <div>
                     <AppBar title={this.props.site} iconElementLeft={<IconButton onTouchTap={()=> this.props.close()}><NavigationClose /></IconButton>} />
                     <Card>
@@ -40,11 +64,26 @@ class SiteDetailsDrawer extends Component {
                             {
                                 <List>
                                    {
-                                       data.sitesWithLogsheet[0].logsheets.map((l) => {
-                                           return( <ListItem 
-                                            key={l.id}
-                                            primaryText={moment(new Date(l.logsheet_date)).format('YYYY MMMM D - dddd')} /> )
-                                       })
+                                        years.map((y) => {
+                                            return(
+                                                <ListItem key={y}
+                                                    primaryText={y} 
+                                                    initiallyOpen={false}
+                                                    primaryTogglesNestedList={true}
+                                                    nestedItems={
+                                                            sortedLogsheets.map((l,index) => {
+                                                                if(new Date(l.logsheet_date).getFullYear() == y) {
+                                                                    return(
+                                                                        <ListItem
+                                                                            key={index}
+                                                                            primaryText={moment(new Date(l.logsheet_date)).format('MMMM D - dddd')}
+                                                                        />
+                                                                    )
+                                                                }
+                                                            })
+                                                        }/>
+                                            )
+                                        })
                                    }
                                 </List>
                             }

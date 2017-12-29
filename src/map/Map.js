94115581ed080ac7fb78930f8d11a9876e513604
oriel <oriel.absin@gmail.com>
2017-12-29 16:10:04 +0800

    add mastercluster group

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { Map, TileLayer } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
 import { client } from '../index'
 import gql from 'graphql-tag';
 
 import 'leaflet/dist/leaflet.css'
+import 'react-leaflet-markercluster/dist/styles.min.css'
 
 class PhMap extends Component {
     constructor() {
@@ -14,33 +16,33 @@ class PhMap extends Component {
           zoom: 6,
           maxZoom: 18,
           minZoom: 6,
-          sites: null
+          sites: client.readQuery({
+                query: gql`
+                    {
+                        sites {
+                            id
+                            name
+                            description
+                            location
+                            longitude
+                            latitude 
+                        }
+                    }
+                `
+            })
         };
     }
 
-    componentDidMount() {
-        const sitesQuery = client.readQuery({
-            query: gql`
-                {
-                    sites {
-                        id
-                        name
-                        description
-                        location
-                        longitude
-                        latitude 
-                    }
-                }
-            `
-        })
-
-        console.log(sitesQuery)
-        this.setState({ sites: sitesQuery })
-
-    }
-
     render() {
-        const { lat, lng, zoom, maxZoom, minZoom } = this.state
+        const { lat, lng, zoom, maxZoom, minZoom, sites } = this.state
+        
+        const markers = sites.sites.filter(s => {
+            return s.latitude
+        }).map(s => {
+            return {
+                position: [s.latitude, s.longitude]
+            }
+        })
 
         return (
             <Map center={[lat, lng]} 
@@ -49,6 +51,8 @@ class PhMap extends Component {
                 <TileLayer
                     attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+
+                <MarkerClusterGroup markers={markers} />
 
             </Map>
         )

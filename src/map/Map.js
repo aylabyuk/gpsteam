00dba268eaef485b72fc9c65fae30dbe373a5cd9
oriel <oriel.absin@gmail.com>
2017-12-29 12:10:17 +0800

    add ph map

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { Map, TileLayer } from 'react-leaflet'
 
-export default class Map extends Component {
+import 'leaflet/dist/leaflet.css'
+
+export default class PhMap extends Component {
+    constructor() {
+        super();
+        this.state = {
+          lat: 12.8797,
+          lng: 121.7740,
+          zoom: 6,
+          maxZoom: 18,
+          minZoom: 6
+        };
+      }
+
     render() {
+        const { lat, lng, zoom, maxZoom, minZoom } = this.state
+
         return (
-            <div>
-                the map
-            </div>
+            <Map center={[lat, lng]} 
+                zoom={zoom} minZoom={minZoom} maxZoom={maxZoom} zoomSnap style={{ height: '100vh' }}>
+
+                <TileLayer
+                    attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+
+
+            </Map>
         )
     }
 }

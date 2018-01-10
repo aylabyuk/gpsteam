18f0b8c3c59e458cead4c16fca6b5ff7d311deeb
oriel <oriel.absin@gmail.com>
2018-01-10 10:49:45 +0800

    sort filtered sites

--- src/map/MapContainer.js
+++ src/map/MapContainer.js
@@ -150,6 +150,13 @@ class Map extends React.Component {
       list = sitesCont.concat(sitesCamp)
     }
 
+    // sort alphabetically
+    list.sort((a, b) => {
+      if(a.name < b.name) return -1;
+      if(a.name > b.name) return 1;
+      return 0;
+    })
+
     const drawerRight = (
       <Drawer
         type="persistent"
